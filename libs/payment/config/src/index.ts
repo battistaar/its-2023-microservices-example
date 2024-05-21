abc22a86f8aff2c880b223f745d253fd20f84e49
Carlo Fabris <carlo.fabris@allievi.itsdigitalacademy.com>
2024-05-21 17:22:09 +0200

    create library payment

---
+++ libs/payment/config/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/payment.config';
+export * from './lib/payment.entities';
+export * from './lib/payment.events';
+
+
---
+++ libs/payment/events/src/lib/payment-events.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { REDIS_HOST, REDIS_PORT } from '@order/config';
+import { OrderEventsService } from '@order/events';
+import { PaymentEventsService } from './payment-events.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PAYMENT_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          host: REDIS_HOST,
+          port: REDIS_PORT,
+        }
+      }
+    ])
+  ],
+  providers: [PaymentEventsService],
+  exports: [PaymentEventsService],
+})
+export class PaymentEventsModule {
+
+
+
+}
