3a72db6ebc6cea28ca85bdbf3be6037da4591b0f
CavaliereDavid <54360776+CavaliereDavid@users.noreply.github.com>
2024-05-28 15:51:11 +0200

    update to notification logger

--- apps/notification/src/app/api/notification.controller.ts
+++ apps/notification/src/app/api/notification.controller.ts
@@ -1,39 +1,40 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
-import { OrderEvent, OrderStatusEvents } from '@order/config';
+import { OrderStatusEvent } from '@order/event-client';
 import { NotificationService } from '../shared/notification.service';
+import { OrderEvent, OrderStatusEvents } from '@order/config';
 
 @Controller()
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
-  @EventPattern('ord_order_status_start')
-  handleStartEvent(@Payload() data: OrderEvent) {
+  @OrderStatusEvent(OrderStatusEvents.START)
+  async handleStartEvent(data: OrderEvent) {
     this.notificationService.sendNotification(OrderStatusEvents.START, data);
   }
 
-  @EventPattern('ord_order_status_payed')
-  handlePayedEvent(@Payload() data: OrderEvent) {
+  @OrderStatusEvent(OrderStatusEvents.PAYED)
+  async handlePayedEvent(data: OrderEvent) {
     this.notificationService.sendNotification(OrderStatusEvents.PAYED, data);
   }
 
-  @EventPattern('ord_order_status_shipping')
-  handleShippingEvent(@Payload() data: OrderEvent) {
+  @OrderStatusEvent(OrderStatusEvents.SHIPPING)
+  async handleShippingEvent(data: OrderEvent) {
     this.notificationService.sendNotification(OrderStatusEvents.SHIPPING, data);
   }
 
-  @EventPattern('ord_order_status_shipped')
-  handleShippedEvent(@Payload() data: OrderEvent) {
+  @OrderStatusEvent(OrderStatusEvents.SHIPPED)
+  async handleShippedEvent(data: OrderEvent) {
     this.notificationService.sendNotification(OrderStatusEvents.SHIPPED, data);
   }
 
-  @EventPattern('ord_order_status_delivered')
-  handleDeliveredEvent(@Payload() data: OrderEvent) {
+  @OrderStatusEvent(OrderStatusEvents.DELIVERED)
+  async handleDeliveredEvent(data: OrderEvent) {
     this.notificationService.sendNotification(OrderStatusEvents.DELIVERED, data);
   }
 
-  @EventPattern('ord_order_status_*')
-  handleAllEvents(@Payload() data: OrderEvent) {
+  @OrderStatusEvent(OrderStatusEvents.ALL)
+  async handleAllEvents(data: OrderEvent) {
     this.notificationService.sendNotification(OrderStatusEvents.ALL, data);
   }
 }
