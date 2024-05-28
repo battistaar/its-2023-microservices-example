bff7bcbd06c8dcbab0530c962da2d49f3bfc6ae8
Matteo Trombin <matteotromb@gmail.com>
2024-05-28 16:27:16 +0200

    fix warehouse lib

--- libs/warehouse/event-client/src/lib/warehouse-event-client.ts
+++ libs/warehouse/event-client/src/lib/warehouse-event-client.ts
@@ -1,11 +1,20 @@
-import { IEventPattern } from "@core/event-utils";
-import { applyDecorators } from "@nestjs/common";
-import { EventPattern, MicroserviceOptions, Transport } from "@nestjs/microservices";
-import { ItemsStatusEvent, REDIS_HOST, REDIS_PORT, WarehouseItem } from "@warehouse/config";
+import { IEventPattern } from '@core/event-utils';
+import { applyDecorators } from '@nestjs/common';
+import {
+  EventPattern,
+  MicroserviceOptions,
+  Transport,
+} from '@nestjs/microservices';
+import {
+  ItemsStatusEvent,
+  REDIS_HOST,
+  REDIS_PORT,
+  WarehouseItem,
+} from '@warehouse/config';
 
 export const ItemsStatusEvents = (event: ItemsStatusEvent) => {
   return applyDecorators(
-    EventPattern(`ord_${event}`, Transport.REDIS)
+    EventPattern(`wh_${event}`, Transport.REDIS)
   ) as IEventPattern<WarehouseItem>;
 };
 
