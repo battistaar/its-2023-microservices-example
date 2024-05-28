643e257e403c4a8dd611baaa8501f7010758030b
Matteo Trombin <matteotromb@gmail.com>
2024-05-28 17:33:49 +0200

    warehouse events service

--- apps/warehouse/src/app/shared/warehouse.service.ts
+++ apps/warehouse/src/app/shared/warehouse.service.ts
@@ -1,40 +1,69 @@
-import { Injectable } from "@nestjs/common";
-import { WarehouseItem } from "@warehouse/config";
+import { Injectable } from '@nestjs/common';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { ShipmentEvent } from '@shipment/config';
+import { ItemsStatusEvent, WarehouseItem } from '@warehouse/config';
+import { WarehouseEventsService } from '@warehouse/events';
+import { WarehouseEvent } from '@warehouse/config';
 
 @Injectable()
 export class WarehouseService {
-    protected products = [
-        {
-          id: '1',
-          quantity: 10
-        },
-        {
-          id: '2',
-          quantity: 20
-        }
-    ];
+  constructor(protected warehouseEventSrv: WarehouseEventsService) {}
 
-    protected workItems: WarehouseItem[] = [];
+  protected products = [
+    {
+      id: '1',
+      quantity: 10,
+    },
+    {
+      id: '2',
+      quantity: 20,
+    },
+  ];
+
+  protected workItems: WarehouseItem[] = [];
+
+  async getStockQuantity(productId: string) {
+    const product = this.products.find((p) => p.id === productId);
+    return product.quantity;
+  }
+
+  async unloadStock(productId: string, quantity: number) {
+    const product = this.products.find((p) => p.id === productId);
+    product.quantity -= quantity;
+
+    return product.quantity;
+  }
+
+  async addWorkItem(payload: ShipmentEvent) {
     
-    async getStockQuantity(productId: string) {
-        const product = this.products.find(p => p.id === productId)
-        return product.quantity;
-    }
-    
-    async unloadStock(productId: string, quantity: number) {
-        const product = this.products.find(p => p.id === productId);
-        product.quantity -= quantity;
-    
-        return product.quantity;
+    const warehouseItem: WarehouseItem = {
+      warehouseId: randomStringGenerator(),
+      orderId: payload.orderId,
+      items: payload.items,
+      shipmentId: payload.shipmentId,
+      shipmentInfo: payload.shipmentInfo,
+      status: "pending"
     }
 
-    async preparingOrder(warehouseId: string) {
-        const item = this.workItems.find(i => i.warehouseId === warehouseId);
-        item.status = 'preparing';
-    }
+    payload.items.forEach((item) => {
+      this.unloadStock(item.productId, item.quantity);
+    })
 
-    async shippedOrder(warehouseId: string) {
-        const item = this.workItems.find(i => i.warehouseId === warehouseId);
-        item.status = 'shipped';
-    }
+    this.workItems.push(warehouseItem);
+  }
+
+  async preparingOrder(warehouseId: string) {
+    const item = this.workItems.find((i) => i.warehouseId === warehouseId);
+    item.status = 'preparing';
+    this.warehouseEventSrv.sendStatusChange(ItemsStatusEvent.PREPARING, item);
+  }
+
+  async shippedOrder(warehouseId: string) {
+    const item = this.workItems.find((i) => i.warehouseId === warehouseId);
+    item.status = 'shipped';
+    
+    const warehouseEvent: WarehouseEvent = { warehouseId: item.warehouseId, orderId: item.orderId, shipmentInfo: item.shipmentInfo, status: item.status };
+
+    this.warehouseEventSrv.sendStatusChange(ItemsStatusEvent.SHIPPED, warehouseEvent);
+  }
 }
--- libs/warehouse/events/src/lib/warehouse-events.service.ts
+++ libs/warehouse/events/src/lib/warehouse-events.service.ts
@@ -1,13 +1,12 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
-import { WarehouseItem, ItemsStatusEvent } from "@warehouse/config";
+import { WarehouseItem, ItemsStatusEvent, WarehouseEvent } from "@warehouse/config";
 
 @Injectable()
 export class WarehouseEventsService { 
     constructor(@Inject('WAREHOUSE_SERVICE') protected warehouseClient: ClientProxy) {}
 
-    sendStatusChange(event: ItemsStatusEvent, data: WarehouseItem) {
-      console.log(`wh_${event}`)
+    sendStatusChange(event: ItemsStatusEvent, data: WarehouseEvent) {
       this.warehouseClient.emit(`wh_${event}`, data);
     }
 }
